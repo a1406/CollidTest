35b0f0bdcb726e9ed00468602fba97a9df7a73c5
tangpl <tangpl@home.com>
2023-08-27 16:07:32 +0800

    1

--- Assets/Scripts/Common.cs
+++ Assets/Scripts/Common.cs
@@ -4,6 +4,42 @@ using UnityEngine;
 
 namespace CollidTest
 {
+    public class Mat33
+    {
+        public Vector3[] Data { get; set; }
+        public Mat33(Vector3 a, Vector3 b, Vector3 c)
+        {
+            Data = new Vector3[3];
+            Data[0] = a;
+            Data[1] = b;
+            Data[2] = c;
+        }
+        public float Det()
+        {
+            //a1，b2，c3
+            //+b1，c2，a3
+            //+c1，a2，b3
+            //-a3，b2，c1
+            //-b3，c2，a1
+            //-c3，a2，b1
+            return Data[0].x * Data[1].y * Data[2].z
+                + Data[1].x * Data[2].y * Data[0].z
+                + Data[2].x * Data[0].y * Data[1].z
+                - Data[0].z * Data[1].y * Data[2].x
+                - Data[1].z * Data[2].y * Data[0].x
+                - Data[2].z * Data[0].y * Data[1].x;
+        }
+
+        public Vector3 CalcLinearEquation(Vector3 p)
+        {
+            float det = Det();
+            Mat33 a = new(p, Data[1], Data[2]);
+            Mat33 b = new(Data[0], p, Data[2]);
+            Mat33 c = new(Data[0], Data[1], p);
+            return new Vector3(a.Det() / det, b.Det() / det, c.Det() / det);
+        }
+
+    }
     public class Plane
     {
         public Vector3 Pos { get; set; }
@@ -81,6 +117,73 @@ namespace CollidTest
             Vector3 t = GetClosestPoint(point) - point;
             return Vector3.Dot(t, t);
         }
+    }
+
+    public class OBB
+    {
+        public Vector3 C { get; set; }
+        public Vector3 AxisX { get; set; }
+        public Vector3 AxisY { get; set; }
+        public Vector3 AxisZ { get; set; }
+
+        public Vector3 HalfLen { get; set; }
+
+        public OBB(Vector3 pos, Vector3 scale, Quaternion rotation)
+        {
+            C = pos;
+            Matrix4x4 rot_mat = Matrix4x4.Rotate(rotation);
+            Vector4 x = new(1, 0, 0, 1);
+            Vector4 y = new(0, 1, 0, 1);
+            Vector4 z = new(0, 0, 1, 1);
+            x = rot_mat * x;
+            y = rot_mat * y;
+            z = rot_mat * z;
+
+            AxisX = new(x.x, x.y, x.z);
+            AxisX.Normalize();
+            AxisY = new(y.x, y.y, y.z);
+            AxisY.Normalize();
+            AxisZ = new(z.x, z.y, z.z);
+            AxisZ.Normalize();
+
+            HalfLen = scale / 2;
+        }
+        public bool GetPoints(out Vector3[] points)
+        {
+            points = new Vector3[8];
+            points[0] = C - AxisX * HalfLen.x + AxisY * HalfLen.y + AxisZ * HalfLen.z;
+            points[1] = C - AxisX * HalfLen.x + AxisY * HalfLen.y - AxisZ * HalfLen.z;
+            points[2] = C + AxisX * HalfLen.x + AxisY * HalfLen.y - AxisZ * HalfLen.z;
+            points[3] = C + AxisX * HalfLen.x + AxisY * HalfLen.y + AxisZ * HalfLen.z;
+
+            points[4] = C - AxisX * HalfLen.x - AxisY * HalfLen.y + AxisZ * HalfLen.z;
+            points[5] = C - AxisX * HalfLen.x - AxisY * HalfLen.y - AxisZ * HalfLen.z;
+            points[6] = C + AxisX * HalfLen.x - AxisY * HalfLen.y - AxisZ * HalfLen.z;
+            points[7] = C + AxisX * HalfLen.x - AxisY * HalfLen.y + AxisZ * HalfLen.z;
+
+            return true;
+        }
+        public Vector3 GetClosestPoint(Vector3 point)
+        {
+            Vector3 pc = point - C;
+            Mat33 mat = new(AxisX, AxisY, AxisZ);
+            Vector3 obb_pos = mat.CalcLinearEquation(pc);
+
+            Vector3 Min, Max;
+            Min = (HalfLen * -1);
+            Max = HalfLen;
+
+            Vector3 ret = obb_pos;
+            if (ret.x < Min.x) ret.x = Min.x;
+            else if (ret.x > Max.x) ret.x = Max.x;
+            if (ret.y < Min.y) ret.y = Min.y;
+            else if (ret.y > Max.y) ret.y = Max.y;
+            if (ret.z < Min.z) ret.z = Min.z;
+            else if (ret.z > Max.z) ret.z = Max.z;
+
+            return (ret.x * AxisX + ret.y * AxisY + ret.z * AxisZ + C);
+        }
+
     }
 }
 
---
+++ Assets/Scripts/point_OBB_5_1_4.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class point_OBB_5_1_4 : MonoBehaviour
+{
+    public Transform point;
+    public Transform OBB;
+    LineRenderer m_lineRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!m_lineRenderer)
+            m_lineRenderer = GetComponent<LineRenderer>();
+        CollidTest.Mat33 test = new(new Vector3(4, 2, 1), new Vector3(5, 3, 7), new Vector3(1, 3, 2));
+        float det = test.Det();
+        Debug.Log($"det = {det}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 start = point.position;
+        Vector3 scale = OBB.localScale;
+        CollidTest.OBB TestOBB = new CollidTest.OBB(OBB.position, scale, OBB.rotation);
+        /*        Vector3[] points;
+                TestOBB.GetPoints(out points);
+                m_lineRenderer.positionCount = 10;
+
+                m_lineRenderer.SetPosition(0, points[0]);
+                m_lineRenderer.SetPosition(1, points[1]);
+                m_lineRenderer.SetPosition(2, points[2]);
+                m_lineRenderer.SetPosition(3, points[3]);
+                m_lineRenderer.SetPosition(4, points[0]);
+
+                m_lineRenderer.SetPosition(5, points[4]);
+
+                m_lineRenderer.SetPosition(6, points[5]);
+                m_lineRenderer.SetPosition(7, points[6]);
+                m_lineRenderer.SetPosition(8, points[7]);
+                m_lineRenderer.SetPosition(9, points[4]);*/
+
+        Vector3 end = TestOBB.GetClosestPoint(start);
+        m_lineRenderer.positionCount = 2;
+
+        m_lineRenderer.SetPosition(0, start);
+        m_lineRenderer.SetPosition(1, end);
+
+        float length = (end - start).sqrMagnitude;
+        Debug.Log($"len = {length}");
+
+
+    }
+}
